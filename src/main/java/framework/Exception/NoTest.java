6ebbf076656a08738de1322d1b668a702c6fac41
Carrol <mnr2@yandex.ru>
2018-04-13 19:20:42 +0300

    c

--- src/main/java/framework/Exception/NoTest.java
+++
@@ -1,7 +0,0 @@
-package framework.Exception;
-
-public class NoTest extends Exception {
-    public NoTest(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/framework/Printer/Writer.java
+++
@@ -1,7 +0,0 @@
-package framework.Printer;
-
-public interface Writer {
-    void writeTestFailed(String msg);
-
-    void writeTestPassed(String msg);
-}
